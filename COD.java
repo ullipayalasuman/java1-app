368f11a4b2f94e8e944ce59a2b007c661a10e8c6
ULLIPAYALASUMAN3289 <sumanchaarvidevops1@gmail.com>
2022-08-27 18:28:58 +0530

    create COD payment

--- COD.java
+++ COD.java
@@ -2,4 +2,17 @@ class COD{
     public static void main(String args[]){  
      System.out.println("Implement COD Functionality");  
     }  
+}  public class ReverseNumberExample1   
+{  
+public static void main(String[] args)   
+{  
+int number = 987654, reverse = 0;  
+while(number != 0)   
+{  
+int remainder = number % 10;  
+reverse = reverse * 10 + remainder;  
+number = number/10;  
+}  
+System.out.println("The reverse of the given number is: " + reverse);  
+}  
 }  
